5b9cbb1c1d8a2bbcc471903a48f9fe100fd36b34
asmaelhasna <163143921+asmaelhasna@users.noreply.github.com>
2025-11-16 20:06:58 +0700

    perbaiki app

--- src/main/java/com/siskema/gryffindor/App.java
+++ src/main/java/com/siskema/gryffindor/App.java
@@ -1,20 +1,19 @@
 package com.siskema.gryffindor;
 
 import javax.swing.SwingUtilities;
-import javax.swing.UIManager; // <-- IMPORT YANG HILANG
+import javax.swing.UIManager; 
 import com.siskema.gryffindor.ui.LoginFrame;
 
 public class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            // Setel Look and Feel ke system default untuk tampilan yang lebih modern
+            
             try {
                 UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
             } catch (Exception e) {
                 e.printStackTrace();
             }
             
-            // Frame yang pertama kali muncul
             new LoginFrame().setVisible(true);
         });
     }
